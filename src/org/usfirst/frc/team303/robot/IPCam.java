66d991b2f838e7fff1c87d08d57c468d4cdc1f35
Bradley <Bradley@Brads-Laptop-2>
2016-12-04 17:36:01 -0500

    IPCam and GRIP Integration
    GRIP does not currently run headless on the RIO, it must be run on a
    co-processor or the driverstation.

--- src/org/usfirst/frc/team303/robot/IPCam.java
+++ src/org/usfirst/frc/team303/robot/IPCam.java
@@ -9,8 +9,14 @@ import com.ni.vision.NIVision.ShapeMode;
 
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.networktables.NetworkTable;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import java.io.IOException;
+
 public class IPCam {
 	
+	double globalCenterY;
+	double globalCenterX;
 	Image frame;
 	Rect overlay;
 	CameraServer server;
@@ -26,12 +32,36 @@ public class IPCam {
 		//overlay = new NIVision.Rect()
 		rect = new NIVision.Rect(10, 10, 100, 100);
 		//server.startAutomaticCapture("cam0");
+		
+		try {
+			new ProcessBuilder("/home/lvuser/grip").inheritIO().start();
+		} catch (IOException e ) {
+			e.printStackTrace();
+		}
+		
 	}
 	
 	public void runCameraServer() {
 		cam.getImage(frame);
 		NIVision.imaqDrawShapeOnImage(frame, frame, rect, DrawMode.DRAW_VALUE, ShapeMode.SHAPE_OVAL, 0.0f);
 		CameraServer.getInstance().setImage(frame);
+		for (double centerX : table.getNumberArray("centerX", new double[0])) {
+           SmartDashboard.putString("Goal Center X", "" + centerX);
+           globalCenterX = centerX;
+           
+        }
+		for (double centerY : table.getNumberArray("centerY", new double[0])) {
+	       SmartDashboard.putString("Goal Center Y", "" + centerY);
+	       globalCenterY = centerY;
+	    }
+	}
+	
+	public double getCenterX() {
+		return globalCenterX;
+	}
+	
+	public double getCenterY() {
+		return globalCenterY;
 	}
 	
 	public double getGoalArea(int arrayNumber) {
